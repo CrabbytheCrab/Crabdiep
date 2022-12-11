c99760ecbe8a5b853e5f2d5d138a27dd820b2abb
CrabbytheCrab <79179107+CrabbytheCrab@users.noreply.github.com>
2022-12-11 18:03:44 -0500

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -101,4 +101,4 @@ export const defaultAccessLevel: AccessLevel = AccessLevel.BetaAccess;
 export const devTokens: Record<string, AccessLevel> = {
     "*": defaultAccessLevel
 }
-export const maxPlayerLevel = 51;
+export const maxPlayerLevel = 75;
