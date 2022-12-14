5fbb58682419e64bf373a09fa6f7bdc4c3c3e685
CrabbytheCrab <79179107+CrabbytheCrab@users.noreply.github.com>
2022-12-14 13:47:19 -0500

    Update Addons.ts

--- src/Entity/Tank/Addons.ts
+++ src/Entity/Tank/Addons.ts
@@ -202,8 +202,8 @@ export class Addon {
             const tickBase = base.tick;
             base.tick = (tick: number) => {
             
-            base.positionData.y = rotator.physicsData.values.size * Math.sin(MAX_ANGLE_RANGE2) * ROT_OFFSET;
-            base.positionData.x = rotator.physicsData.values.size * Math.cos(MAX_ANGLE_RANGE2) * ROT_OFFSET;
+            base.positionData.y = this.owner.physicsData.values.size * Math.sin(MAX_ANGLE_RANGE2) * ROT_OFFSET;
+            base.positionData.x = this.owner.physicsData.values.size * Math.cos(MAX_ANGLE_RANGE2) * ROT_OFFSET;
             //base.positionData.values.x += rotator.physicsData.values.size * Math.cos(MAX_ANGLE_RANGE2)  * ROT_OFFSET;
 
                 tickBase.call(base, tick);
