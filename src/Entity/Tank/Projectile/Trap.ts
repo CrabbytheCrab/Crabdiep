338f3fa6372cf4ea3be863f8eaf153ac4a75c060
CrabbytheCrab <79179107+CrabbytheCrab@users.noreply.github.com>
2022-12-15 13:32:31 -0500

    Update Trap.ts

--- src/Entity/Tank/Projectile/Trap.ts
+++ src/Entity/Tank/Projectile/Trap.ts
@@ -15,7 +15,7 @@
     You should have received a copy of the GNU Affero General Public License
     along with this program. If not, see <https://www.gnu.org/licenses/>
 */
-
+import LivingEntity from "../../Live";
 import Barrel from "../Barrel";
 import Bullet from "./Bullet";
 
