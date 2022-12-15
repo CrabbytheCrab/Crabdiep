59361ceb53159ce7efe2bba26894221906f4ea3c
CrabbytheCrab <79179107+CrabbytheCrab@users.noreply.github.com>
2022-12-15 08:01:45 -0500

    Update Addons.ts

--- src/Entity/Tank/Addons.ts
+++ src/Entity/Tank/Addons.ts
@@ -192,9 +192,8 @@ export class Addon {
                 return deltaAngle < MAX_ANGLE_RANGE || deltaAngle > (PI2 - MAX_ANGLE_RANGE);
             }
 
-            base.positionData.values.y = this.owner.physicsData.values.size * Math.sin(MAX_ANGLE_RANGE) * ROT_OFFSET;
-            base.positionData.values.x = this.owner.physicsData.values.size * Math.cos(MAX_ANGLE_RANGE) * ROT_OFFSET;
-
+            base.positionData.values.y = this.owner.physicsData.values.size * Math.sin(angle) * ROT_OFFSET;
+            base.positionData.values.x = this.owner.physicsData.values.size * Math.cos(angle) * ROT_OFFSET;
 
         // if (base.styleData.values.flags & StyleFlags.showsAboveParent) base.styleData.values.flags ^= StyleFlags.showsAboveParent;
             base.physicsData.values.flags |= PositionFlags.absoluteRotation;
@@ -202,8 +201,8 @@ export class Addon {
             const tickBase = base.tick;
             base.tick = (tick: number) => {
             
-            base.positionData.y = this.owner.physicsData.values.size * Math.sin(MAX_ANGLE_RANGE) * ROT_OFFSET;
-            base.positionData.x = this.owner.physicsData.values.size * Math.cos(MAX_ANGLE_RANGE) * ROT_OFFSET;
+            base.positionData.y = this.owner.physicsData.values.size * Math.sin(angle) * ROT_OFFSET;
+            base.positionData.x = this.owner.physicsData.values.size * Math.cos(angle) * ROT_OFFSET;
             //base.positionData.values.x += rotator.physicsData.values.size * Math.cos(MAX_ANGLE_RANGE2)  * ROT_OFFSET;
 
                 tickBase.call(base, tick);
