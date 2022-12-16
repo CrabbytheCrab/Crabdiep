d17306428a2ade304221da68ee23014d49c5264c
CrabbytheCrab <79179107+CrabbytheCrab@users.noreply.github.com>
2022-12-16 10:56:36 -0500

    Update Hive.ts

--- src/Entity/Tank/Projectile/Hive.ts
+++ src/Entity/Tank/Projectile/Hive.ts
@@ -90,13 +90,45 @@ export default class Hive extends Bullet {
     public tick(tick: number) {
         const usingAI = !this.canControlDrones || this.tank.inputs.deleted || (!this.tank.inputs.attemptingShot() && !this.tank.inputs.attemptingRepel());
         const inputs = !usingAI ? this.tank.inputs : this.ai.inputs;
+
+        if (usingAI && this.ai.state === AIState.idle) {
+            const delta = {
+                x: this.positionData.values.x - this.tank.positionData.values.x,
+                y: this.positionData.values.y - this.tank.positionData.values.y
+            }
+            const base = this.baseAccel;
+
+            // still a bit inaccurate, works though
+            let unitDist = (delta.x ** 2 + delta.y ** 2) / Drone.MAX_RESTING_RADIUS;
+           /* if (unitDist <= 1 && this.restCycle) {
+                this.baseAccel /= 6;
+                this.positionData.angle += 0.01 + 0.012 * unitDist;
+            } else {
+                const offset = Math.atan2(delta.y, delta.x) + Math.PI / 2
+                delta.x = this.tank.positionData.values.x + Math.cos(offset) * this.tank.physicsData.values.size * 1.2 - this.positionData.values.x;
+                delta.y = this.tank.positionData.values.y + Math.sin(offset) * this.tank.physicsData.values.size * 1.2 - this.positionData.values.y;
+                this.positionData.angle = Math.atan2(delta.y, delta.x);
+                if (unitDist < 0.5) this.baseAccel /= 3;
+                this.restCycle = (delta.x ** 2 + delta.y ** 2) <= 4 * (this.tank.physicsData.values.size ** 2);
+            }*/
+
             if (!Entity.exists(this.barrelEntity)) this.destroy();
 
             this.tickMixin(tick);
 
+            this.baseAccel = base;
 
-            //this.positionData.angle = Math.atan2(inputs.mouse.y - this.positionData.values.y, inputs.mouse.x - this.positionData.values.x);
+            return;
+        } else {
+            this.positionData.angle = Math.atan2(inputs.mouse.y - this.positionData.values.y, inputs.mouse.x - this.positionData.values.x);
             this.restCycle = false
+        }
+
+
+        
+        if (this.canControlDrones && inputs.attemptingRepel()) {
+            this.positionData.angle += Math.PI; 
+        }
 
         // So that switch tank works, as well as on death
         if (!Entity.exists(this.barrelEntity)) this.destroy();
