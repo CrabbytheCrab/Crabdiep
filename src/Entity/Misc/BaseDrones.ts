64dc70790637e1ac769de1c1845063dceaefb306
Nulled <63370961+Nul-led@users.noreply.github.com>
2022-12-29 21:31:36 +0100

    Update BaseDrones.ts

--- src/Entity/Misc/BaseDrones.ts
+++ src/Entity/Misc/BaseDrones.ts
@@ -16,7 +16,193 @@
     along with this program. If not, see <https://www.gnu.org/licenses/>
 */
 
-// Hi there
+import { TankDefinition } from "../../Const/TankDefinitions"
+import { CameraEntity } from "../../Native/Camera"
+import { Inputs } from "../AI"
+import Barrel from "../Tank/Barrel"
+import TankBody from "../Tank/TankBody"
+import TeamBase from "./TeamBase"
 
-// TODO(ABC):
-// Implement 8 drones owned by an invisible overlord
+const tankdef: TankDefinition = {
+    "id": 0,
+    "name": "",
+    "upgradeMessage": "",
+    "levelRequirement": 0,
+    "upgrades": [],
+    "flags": {
+        "invisibility": false,
+        "zoomAbility": false,
+        "canClaimSquares": false,
+        "devOnly": true
+    },
+    "visibilityRateShooting": 0.23,
+    "visibilityRateMoving": 0.08,
+    "invisibilityRate": 0.03,
+    "fieldFactor": 0.4,
+    "absorbtionFactor": 1,
+    "speed": 0,
+    "maxHealth": 1,
+    "preAddon": null,
+    "postAddon": null,
+    "sides": 0,
+    "borderWidth": 15,
+    "barrels": [
+        {
+            "angle": -1.5707963267948966,
+            "offset": 0,
+            "size": 70,
+            "width": 42,
+            "delay": 0,
+            "reload": 6,
+            "recoil": 1,
+            "isTrapezoid": true,
+            "trapezoidDirection": 0,
+            "addon": null,
+            "droneCount": 2,
+            "canControlDrones": true,
+            "bullet": {
+                "type": "basedrone",
+                "sizeRatio": 1,
+                "health": 100,
+                "damage": 1.5,
+                "speed": 2.5,
+                "scatterRate": 1,
+                "lifeLength": -1,
+                "absorbtionFactor": 1
+            }
+        },
+        {
+            "angle": 1.5707963267948966,
+            "offset": 0,
+            "size": 70,
+            "width": 42,
+            "delay": 0,
+            "reload": 6,
+            "recoil": 1,
+            "isTrapezoid": true,
+            "trapezoidDirection": 0,
+            "addon": null,
+            "droneCount": 2,
+            "canControlDrones": true,
+            "bullet": {
+                "type": "basedrone",
+                "sizeRatio": 1,
+                "health": 100,
+                "damage": 1.5,
+                "speed": 2.5,
+                "scatterRate": 1,
+                "lifeLength": -1,
+                "absorbtionFactor": 1
+            }
+        },
+        {
+            "angle": 0,
+            "offset": 0,
+            "size": 70,
+            "width": 42,
+            "delay": 0,
+            "reload": 6,
+            "recoil": 1,
+            "isTrapezoid": true,
+            "trapezoidDirection": 0,
+            "addon": null,
+            "droneCount": 2,
+            "canControlDrones": true,
+            "bullet": {
+                "type": "basedrone",
+                "sizeRatio": 1,
+                "health": 100,
+                "damage": 1.5,
+                "speed": 2.5,
+                "scatterRate": 1,
+                "lifeLength": -1,
+                "absorbtionFactor": 1
+            }
+        },
+        {
+            "angle": 3.141592653589793,
+            "offset": 0,
+            "size": 70,
+            "width": 42,
+            "delay": 0,
+            "reload": 6,
+            "recoil": 1,
+            "isTrapezoid": true,
+            "trapezoidDirection": 0,
+            "addon": null,
+            "droneCount": 2,
+            "canControlDrones": true,
+            "bullet": {
+                "type": "basedrone",
+                "sizeRatio": 1,
+                "health": 100,
+                "damage": 1.5,
+                "speed": 2.5,
+                "scatterRate": 1,
+                "lifeLength": -1,
+                "absorbtionFactor": 1
+            }
+        }
+    ],
+    "stats": [
+        {
+            "name": "Movement Speed",
+            "max": 7
+        },
+        {
+            "name": "Reload",
+            "max": 7
+        },
+        {
+            "name": "Drone Damage",
+            "max": 7
+        },
+        {
+            "name": "Drone Health",
+            "max": 7
+        },
+        {
+            "name": "Drone Speed",
+            "max": 7
+        },
+        {
+            "name": "Body Damage",
+            "max": 7
+        },
+        {
+            "name": "Max Health",
+            "max": 7
+        },
+        {
+            "name": "Health Regen",
+            "max": 7
+        }
+    ]
+}
+
+export default class BaseDrones extends TankBody {
+    public constructor(owner: TeamBase, xOffset: number, yOffset: number) {
+        super(owner.game, new CameraEntity(owner.game), new Inputs());
+
+        this.positionData.x = owner.positionData.x + xOffset;
+        this.positionData.y = owner.positionData.y + yOffset;
+
+        for (let i = 0; i < this.children.length; ++i) {
+            this.children[i].isChild = false;
+            this.children[i].delete();
+        }
+        this.children = [];
+        this.barrels = [];
+
+        this.definition = tankdef;
+
+        this.physicsData.sides = 0;
+        this.styleData.opacity = 0;
+        this.relationsData.team = owner.relationsData.team;
+        this.styleData.color = owner.styleData.color;
+
+        for (const barrel of tankdef.barrels) {
+            this.barrels.push(new Barrel(this, barrel));
+        }
+    }
+}
