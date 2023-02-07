a79ac8658f9fd439ac52fe48d95cf3053b7abdf1
CrabbytheCrab <79179107+CrabbytheCrab@users.noreply.github.com>
2023-02-07 13:11:37 -0500

    Update BarrelAddons.ts

--- src/Entity/Tank/BarrelAddons.ts
+++ src/Entity/Tank/BarrelAddons.ts
@@ -62,16 +62,16 @@ export class TrapLauncher extends ObjectEntity {
         //this.positionData.values.angle = Math.PI;
 
         this.physicsData.values.sides = 2;
-        this.physicsData.values.width = barrel.physicsData.values.width;
-        this.physicsData.values.size = barrel.physicsData.values.width * (20 / 42);
+        this.physicsData.values.width = barrel.physicsData.values.width/1.75 * 1.25;
+        this.physicsData.values.size = barrel.physicsData.values.width * (10 / 42);
         this.positionData.values.x = (barrel.physicsData.values.size + this.physicsData.values.size) / 2;
     }
 
     public resize() {
         this.styleData.color = this.barrelEntity.styleData.color;
         this.physicsData.sides = 2;
-        this.physicsData.width = this.barrelEntity.physicsData.values.width;
-        this.physicsData.size = this.barrelEntity.physicsData.values.width * (20 / 42);
+        this.physicsData.width = this.barrelEntity.physicsData.values.width/1.75 * 1.25;
+        this.physicsData.size = this.barrelEntity.physicsData.values.width * (10 / 42);
         this.positionData.x = (this.barrelEntity.physicsData.values.size + this.physicsData.values.size) / 2;
     }
 
