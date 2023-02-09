93ece8f179cb4309915e893e5bbc9431306effbf
CrabbytheCrab <79179107+CrabbytheCrab@users.noreply.github.com>
2023-02-09 09:34:34 -0500

    Update TankBody.ts

--- src/Entity/Tank/TankBody.ts
+++ src/Entity/Tank/TankBody.ts
@@ -1,17 +1,14 @@
 /*
     DiepCustom - custom tank game server that shares diep.io's WebSocket protocol
     Copyright (C) 2022 ABCxFF (github.com/ABCxFF)
-
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU Affero General Public License as published
     by the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
-
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU Affero General Public License for more details.
-
     You should have received a copy of the GNU Affero General Public License
     along with this program. If not, see <https://www.gnu.org/licenses/>
 */
@@ -455,10 +452,10 @@ public canchain: boolean
             const a = this.segments[i - 1];
             const b = this.segments[i];
             /*const delta = {
-                x: a.positionData.values.x - b.positionData.values.x * 1.5,
-                y: a.positionData.values.y - b.positionData.values.y * 1.5
+                x: a.positionData.values.x - b.positionData.values.x,
+                y: a.positionData.values.y - b.positionData.values.y
             }*/
-            const delta = new Vector((a.positionData.values.x - b.positionData.values.x), (a.positionData.values.x - b.positionData.values.y));
+            const delta = new Vector(a.positionData.values.x - b.positionData.values.x, a.positionData.values.y - b.positionData.values.y);
             const x = delta.magnitude - Math.max(a.restLength, b.restLength);
       
             let force = delta.unitVector.scale(-this.k * x);
