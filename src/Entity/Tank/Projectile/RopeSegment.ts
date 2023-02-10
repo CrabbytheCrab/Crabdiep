59e13dce7dfd05bf8d67b6baa6fea1ca429c7d82
CrabbytheCrab <79179107+CrabbytheCrab@users.noreply.github.com>
2023-02-10 13:17:56 -0500

    Update RopeSegment.ts

--- src/Entity/Tank/Projectile/RopeSegment.ts
+++ src/Entity/Tank/Projectile/RopeSegment.ts
@@ -118,6 +118,8 @@ export default class RopeSegment extends LivingEntity implements BarrelBase {
         this.physicsData.pushFactor = 10;
                 this.physicsData.sides = 6;    
                     this.damagePerTick = 10
+            if(this.parent.inputs.attemptingShot()){
+            this.velocity = Vector(0,0)}
         this.physicsData.size = this.parent.physicsData.size
         }else{
                         if(this.CanSpawn){
