d85625e302e92c2a0583c8f028b36edbbbe8572d
CrabbytheCrab <79179107+CrabbytheCrab@users.noreply.github.com>
2023-02-15 08:24:36 -0500

    Update TankBody.ts

--- src/Entity/Tank/TankBody.ts
+++ src/Entity/Tank/TankBody.ts
@@ -464,7 +464,7 @@ public canchain: boolean
       
             let force = delta.unitVector.scale(-this.k * x);
       
-            if (a.isAffectedByRope)a.addAcceleration(force.angle, force.magnitude), false);
+            if (a.isAffectedByRope)a.addAcceleration(force.angle, force.magnitude, false);
       
             force = force.scale(-1);
 
