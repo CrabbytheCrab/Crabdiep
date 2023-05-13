e50fe9f0827040b4d2d3a68413465551124b780c
( ͡° ͜ʖ ͡°) Fs unlimited <faris.soliman@yahoo.com>
2023-05-13 10:09:17 -0400

    Update Ball.ts

--- src/Gamemodes/Misc/Ball.ts
+++ src/Gamemodes/Misc/Ball.ts
@@ -151,7 +151,7 @@ this.BlueScore = 0
         ball.physicsData.flags |= PhysicsFlags.showsOnMap
         ball.physicsData.values.size = 80;
         ball.physicsData.values.absorbtionFactor = 1;
-        ball.damagePerTick = 1000
+        ball.damagePerTick = 0.3
         ball.physicsData.pushFactor = 4
         
         ball.damageReduction = 0
